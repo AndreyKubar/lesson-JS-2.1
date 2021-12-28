825487cccc33cb47cae95044954362722efe345b
AndreyKubar <94114933+AndreyKubar@users.noreply.github.com>
2021-12-28 12:26:06 +0300

    Add files via upload

---
+++ lesson_2.1.js
@@ -0,0 +1,12 @@
+function clickBtn() {
+    alert('уря!')
+}
+
+function blurBtn() {
+    alert('ждем возвращения')
+}
+
+function onClickBtn() {
+    let renameText = document.getElementById("myinput").value
+    document.getElementById('text').value = renameText;
+}
